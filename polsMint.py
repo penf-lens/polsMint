5100f1b8893bde9266adb70b3c1829195c18a091
penf-lens <111521976+penf-lens@users.noreply.github.com>
2023-12-18 16:52:11 +0800

    Update polsMint.py
    WEMIX

--- polsMint.py
+++ polsMint.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 private_key = os.environ.get('account_private_key')
 adress = os.environ.get('account_address')
-rpc_url = "https://1rpc.io/matic"
+rpc_url = "https://api.wemix.com"
 web3 = Web3(Web3.HTTPProvider(rpc_url))
 print(web3.is_connected()) 
 print(Web3.from_wei(web3.eth.get_balance(adress),'ether')) 
@@ -14,10 +14,10 @@ while True:
     nonce = web3.eth.get_transaction_count(adress)
     tx = {
         'nonce': nonce,
-        'chainId': 137,
+        'chainId': 1111,
         'to': adress, 
         'from':adress,
-        'data':'0x646174613a2c7b2270223a227072632d3230222c226f70223a226d696e74222c227469636b223a22706f6c73222c22616d74223a22313030303030303030227d', #mint 16进制数据
+        'data':'646174613a2c7b2270223a227772632d3230222c226f70223a226d696e74222c227469636b223a2277656d6973222c22616d74223a2231303030227d', #mint 16进制数据
         'gasPrice': web3.eth.gas_price,
         'value': Web3.to_wei(0, 'ether') 
     }
